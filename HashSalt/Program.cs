97c44e641fe647bb463e8ad51e85434d7693fffd
wendyconditions <wendymaldonado.la@gmail.com>
2018-01-19 12:14:47 -0800

    First commit

---
+++ HashSalt/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNet.Cryptography.KeyDerivation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashSalt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a password: ");
+            string password = Console.ReadLine();
+
+            byte[] salt = new byte[128 / 8];
+
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(salt);
+            }
+
+            Console.WriteLine($"Salt: {Convert.ToBase64String(salt)}");
+
+            // derive
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA1,
+                iterationCount: 10000,
+                numBytesRequested: 256 / 8));
+
+            Console.WriteLine($"Hashed: {hashed}");
+        }
+    }
+}
